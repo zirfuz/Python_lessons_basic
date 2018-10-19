fbce91f9839791c586d3bfea95370a67d437af42
AO7 <asdf7@asdf.asdf>
2018-10-20 01:33:59 +0300

    Easy, Normal

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -2,43 +2,55 @@ import os
 import sys
 import shutil
 
+def make_dir(name):
+  dir_path = os.path.join(os.getcwd(), name)
+  os.mkdir(dir_path)
+
+def remove_dir(name):
+  dir_path = os.path.join(os.getcwd(), name)
+  os.rmdir(dir_path)
+
+def print_cur_dir():
+  print(list(filter(os.path.isdir, os.listdir())))
+
+if __name__ == '__main__':
+
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
-print('=== 1 ===')
+  print('=== 1 ===')
 
-def make_dirs():
-  for i in range(1, 10):
-    name = 'dir_' + str(i)
-    dir_path = os.path.join(os.getcwd(), name)
-    try:
-      os.mkdir(dir_path)
-    except FileExistsError:
-      pass
+  def make_dirs():
+    for i in range(1, 10):
+      name = 'dir_' + str(i)
+      try:
+        make_dir(name)
+      except FileExistsError:
+        pass
 
-def remove_dirs():
-  for i in range(1, 10):
-    name = 'dir_' + str(i)
-    dir_path = os.path.join(os.getcwd(), name)
-    try:
-      os.rmdir(dir_path)
-    except FileNotFoundError:
-      pass
+  def remove_dirs():
+    for i in range(1, 10):
+      name = 'dir_' + str(i)
+      try:
+        remove_dir(name)
+      except FileNotFoundError:
+        pass
 
-make_dirs()
-remove_dirs()
+
+  make_dirs()
+  remove_dirs()
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
-print('\n=== 2 ===')
-print(list(filter(os.path.isdir, os.listdir())))
+  print('\n=== 2 ===')
+  print_cur_dir()
 
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 
-print('\n=== 3 ===')
-shutil.copyfile(sys.argv[0], 'copy of ' + os.path.basename(sys.argv[0]))
+  print('\n=== 3 ===')
+  shutil.copyfile(sys.argv[0], 'copy of ' + os.path.basename(sys.argv[0]))
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -1,3 +1,6 @@
+import os
+from hw05_easy import make_dir, remove_dir, print_cur_dir
+
 # Задача-1:
 # Напишите небольшую консольную утилиту,
 # позволяющую работать с папками текущей директории.
@@ -13,3 +16,48 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+while True:
+  print('---------------------------------------\n' +
+        '1. Перейти в папку\n' +
+        '2. Просмотреть содержимое текущей папки\n' +
+        '3. Удалить папку\n' +
+        '4. Создать папку\n' +
+        '---------------------------------------')
+
+  try:
+    sel = int(input('Input: '))
+    if sel < 1 or sel > 4:
+      raise RuntimeError()
+  except:
+    print('Bad input!\n')
+    continue
+
+  if sel == 1:
+    fol = input('chdir: ')
+    try:
+      os.chdir(fol)
+      print('Успешно перешел')
+    except:
+      print('Невозможно перейти')
+
+  elif sel == 2:
+    print_cur_dir()
+
+  elif sel == 3:
+    fol = input('Folder name: ')
+    try:
+      remove_dir(fol)
+      print('Успешно удалено')
+    except:
+      print('Невозможно удалить')
+
+  elif sel == 4:
+    fol = input('Folder name: ')
+    try:
+      make_dir(fol)
+      print('Успешно создано')
+    except:
+      print('Невозможно создать')
+
+  print()
