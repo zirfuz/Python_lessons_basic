cee73753cc1dc8487c478bf66937d6c077d03c39
AO7 <asdf7@asdf.asdf>
2018-10-27 06:00:25 +0300

    Generator and ticket

--- lesson07/home_work/loto.py
+++ lesson07/home_work/loto.py
@@ -57,3 +57,55 @@
 модуль random: http://docs.python.org/3/library/random.html
 
 """
+
+import random
+
+class Generator:
+    def __init__(self, min, max):
+        self.__min = min
+        self.__max = max
+        self.__remain = [i for i in range(max + 1)]
+        self.__remain_count = max
+
+    def __call__(self):
+        if self.__remain_count < self.__min:
+            return None
+        r = random.randint(self.__min, self.__remain_count)
+        ret = self.__remain[r]
+        self.__remain[r], self.__remain[self.__remain_count] = \
+            self.__remain[self.__remain_count], self.__remain[r]
+        self.__remain_count -= 1
+        return ret
+
+
+class Ticket: # None: empty, 0: crossed
+    def __init__(self, gen):
+        self.__cells = [None] * 27
+        for line_begin in range(0, 27, 9):
+            gen_indexes = Generator(line_begin, line_begin + 8)
+            numbers = [gen() for _ in range(5)]
+            indexes = [gen_indexes() for _ in range(5)]
+            numbers.sort()
+            indexes.sort()
+            for i in range(5):
+                self.__cells[indexes[i]] = numbers[i]
+
+    def str(self):
+        ret = '-' * 26 + '\n'
+        for i, cell in enumerate(self.__cells):
+            s = ''
+            if cell is not None:
+                s = '-' if cell == 0 else str(cell)
+            frmt = '%2s' if i % 9 == 0 else '%3s'
+            ret += frmt % s
+            if (i + 1) % 9 == 0:
+                ret += '\n'
+        ret += '-' * 26 + '\n'
+        return ret
+
+gen = Generator(1, 90)
+ticket = Ticket(gen)
+print(ticket.str())
+
+# gen = Generator(1,5)
+# print([gen()]*5)
